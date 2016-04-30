fc1947783894057a5011020b0019132f2fffab82
Zoffix Znet <cpan@zoffix.com>
2016-04-29 22:59:48 -0400

    Stuff

--- app.p6
+++ app.p6
@@ -1,16 +1,18 @@
 use Bailador;
 use Bailador::Plugin::Static;
 use GlotIO;
+use JSON::Fast;
+use URI::Escape;
 
 my GlotIO $glot .= new: :key<045e1a1a-6b16-40b5-81ba-08a5660081c5>;
 
+get '/' => sub { 'index.html'.IO };
+get '/main.js' => sub { 'main.js'.IO };
+
 post '/run' => sub {
-    #my $c = request.params<code>;
-    #say request.params;
-    #say $c;
-    return 'looks good';
-    app.headers<Access-Control-Allow-Origin> = '*';
-    #'{foo:"bar"}';
+    my $c = uri-unescape request.params<code>;
+    my $res= $glot.run: 'perl6', $c;
+    return qq|{out: "$res<out>"}|;
 }
 
 baile;
--- main.js
+++ main.js
@@ -9,20 +9,11 @@ function setup_glot_io() {
     codes.each(function(i, el){
         $(el).find('button').click(function() {
             var code  = $(el).parent('.glot-code').find('textarea').val();
-            $.ajax({
-                type: "POST",
-                // url: 'http://httpbin.org/post',
-                url: 'http://localhost:3000/run',
-                data: 'code={"files": [{"name": "main.p6", "content": "print(42)"}]}',
-                success: function(data){alert("yey!")},
-                dataType: 'text/html'
-            });
+            $.post('http://localhost:3000/run',
+                'code=say%20print(42)',
+                function(data){alert("wtf?" + data)}
+            );
             return false;
-//             $.post('http://localhost:3000/run',
-//                 'code={"files": [{"name": "main.p6", "content": "print(42)"}]}',
-//                 function(data){alert("yey!" + data)},
-//                 'text/html'
-//             );
         })
     });
 }
